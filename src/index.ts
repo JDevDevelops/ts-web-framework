6d7486e3cb47cc7fd65fbe66a19548bcc78fd921
MDTCL13 <mdtcl13@Derricks-iMac.local>
2025-05-05 12:24:51 -0500

    wrote some tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import { Collection } from './models/Collection';
 import { User } from './models/User';
 
+const rootURL = 'http://localhost:3000/users';
+
+const collection = new Collection(rootURL);
 const user = User.build({ id: '869a' });
 
+collection.on('change', () => console.log(collection));
 user.on('change', () => console.log(user));
 
+collection.fetch();
 user.fetch();
